2db3ce504cfef038f8a1cc6dd888642ba45d8500
shaahfardin <fardinshaah@gmail.com>
2022-11-08 11:08:52 +0600

    Uri string username and password changed with .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,7 +13,7 @@ app.use(express.json());
 // DB_PASSWORD=photographyUser123
 
 
-const uri = "mongodb+srv://photographyUser:photographyUser123@cluster0.il5mbbt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.il5mbbt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
