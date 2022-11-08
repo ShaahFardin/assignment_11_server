f84befbb5d88d28efd8e67af66f6b376a61a6bb1
shaahfardin <fardinshaah@gmail.com>
2022-11-08 11:29:51 +0600

    Services api created

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// DB_USER=photographyUser
-// DB_PASSWORD=photographyUser123
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.il5mbbt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,6 +21,29 @@ async function run() {
     }
 }
 run();
+
+// db name : Photography
+
+const servicesCollection = client.db('Photography').collection('services');
+
+app.get('/services', async (req, res) => {
+    const services = await servicesCollection.find({}).toArray();
+
+    try {
+        res.send({
+            success: true,
+            message: "Got the data successfully",
+            data: services
+        })
+    } catch (error) {
+        console.log(error.name, error.message, error.stack);
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+})
+
 
 app.get('/', (req, res) => {
     res.send("Server is up and running");
