f283be9d3ac77a0cb2f35e24cdd738e90e72e401
shaahfardin <fardinshaah@gmail.com>
2022-11-08 10:58:48 +0600

    Database connected with server

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+
+// DB_USER=photographyUser
+// DB_PASSWORD=photographyUser123
+
+
+const uri = "mongodb+srv://photographyUser:photographyUser123@cluster0.il5mbbt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log(`Database connected at , ${uri}`);
+    } catch (err) {
+        console.log(err.name, err.message, err.stack);
+    }
+}
+run();
+
+app.get('/', (req, res) => {
     res.send("Server is up and running");
 });
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Assignment-11-server is running on port ${port}`);
 })
